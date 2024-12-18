cfbf18788b04da201de567912612dba246015ad7
meshs1 <191111348+meshs1@users.noreply.github.com>
2024-12-18 12:19:46 +0200

    Remove back buttons

--- app/src/main/java/com/myvet/myvet/Pet_signUp.kt
+++ app/src/main/java/com/myvet/myvet/Pet_signUp.kt
@@ -6,31 +6,39 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.firebase.firestore.FirebaseFirestore
 
 
-class Pet_signUp  : AppCompatActivity() {
-
-   private lateinit var Back_Btn: Button
-   private lateinit var pet_owner_name: EditText
-   private lateinit var petName: EditText
-   private lateinit var password: EditText
-   private lateinit var email: EditText
-   private lateinit var address: EditText
-   private lateinit var agePet: EditText
-   private lateinit var Historical_medical: EditText
-   private lateinit var register: Button
-   private lateinit var errorMessage: TextView
+class Pet_signUp : AppCompatActivity() {
+    private lateinit var pet_owner_name: EditText
+    private lateinit var petName: EditText
+    private lateinit var password: EditText
+    private lateinit var email: EditText
+    private lateinit var address: EditText
+    private lateinit var agePet: EditText
+    private lateinit var Historical_medical: EditText
+    private lateinit var register: Button
+    private lateinit var errorMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val intent = Intent(this@Pet_signUp, SignUp::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+                finish()
+            }
+        })
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_pet_signup)
 
-        Back_Btn = findViewById(R.id.back_home)
         pet_owner_name = findViewById(R.id.user_name_pet)
         password = findViewById(R.id.password_pet)
         email = findViewById(R.id.email_pet)
@@ -43,15 +51,9 @@ class Pet_signUp  : AppCompatActivity() {
 
         val db = FirebaseFirestore.getInstance()
 
-        Back_Btn.setOnClickListener {
-            Log.i("Test back", "The user will go back to home page")
-            val intent = Intent(this, SignUp::class.java)
-            startActivity(intent)
-            finish()
-        }
-
         //Set the register button to be disabled
         register.isEnabled = false
+
         //Function to check if the user has typed in all the information needed for the registration
         fun checkInputs() {
             val pet_owner_name = pet_owner_name.text.toString()
@@ -105,10 +107,16 @@ class Pet_signUp  : AppCompatActivity() {
                         )
                         db.collection("pet owner").document(email.text.toString()).set(user)
                             .addOnSuccessListener {
-                                Log.i("Registration pet owner", "The user has been added to the database")
+                                Log.i(
+                                    "Registration pet owner",
+                                    "The user has been added to the database"
+                                )
                             }
                             .addOnFailureListener {
-                                Log.i("Registration pet owner", "The user has not been added to the database")
+                                Log.i(
+                                    "Registration pet owner",
+                                    "The user has not been added to the database"
+                                )
                             }
                         val intent = Intent(this, MainActivity::class.java)//change to pet page
                         startActivity(intent)
@@ -117,8 +125,5 @@ class Pet_signUp  : AppCompatActivity() {
                 }
 
         }
-
-
-
     }
 }
