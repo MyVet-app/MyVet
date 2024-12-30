2c9b9df9044a7e4069cfd2ca7e9638929f0a384e
meshs1 <191111348+meshs1@users.noreply.github.com>
2024-12-30 10:55:20 +0200

    Add "add availability" button to vet view

--- app/src/main/java/com/myvet/myvet/VetWindow.kt
+++ app/src/main/java/com/myvet/myvet/VetWindow.kt
@@ -1,12 +1,15 @@
 package com.myvet.myvet
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class VetWindow : AppCompatActivity() {
+    private lateinit var addAvailability: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +20,18 @@ class VetWindow : AppCompatActivity() {
 
         val textView: TextView = findViewById(R.id.HelloText)
         textView.text = "Welcome ${user?.displayName}"
+
+        addAvailability = findViewById(R.id.AddAvailability)
+        addAvailability.setOnClickListener {
+            val intent = Intent(this, AddAvailability::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+// Availability:
+// {
+//      "vet": vetid,
+//      "date": ...,
+//      "window": time range (start time - end time),
+// }
