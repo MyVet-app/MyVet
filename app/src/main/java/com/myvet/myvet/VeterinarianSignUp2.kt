863afc7397ccd5e8185679fc907367db5ddb0e68
rony.levy1 <roniki04@gmail.com>
2025-02-09 13:26:40 +0200

    vet signUp, finish

--- app/src/main/java/com/myvet/myvet/VeterinarianSignUp2.kt
+++ app/src/main/java/com/myvet/myvet/VeterinarianSignUp2.kt
@@ -1,90 +1,3 @@
-//import android.content.Intent
-//import android.os.Bundle
-//import android.os.Handler
-//import android.os.Looper
-//import android.util.Log
-//import android.widget.Button
-//import android.widget.EditText
-//import android.widget.TextView
-//import android.widget.Toast
-//import androidx.activity.OnBackPressedCallback
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.widget.addTextChangedListener
-//import com.firebase.geofire.GeoFireUtils
-//import com.firebase.geofire.GeoLocation
-//import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.firestore.FirebaseFirestore
-//import com.myvet.myvet.NominatimResult
-//import com.myvet.myvet.R
-//import com.myvet.myvet.SignUp
-//import com.squareup.moshi.Moshi
-//import com.squareup.moshi.Types
-//import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-//import okhttp3.Call
-//import okhttp3.Callback
-//import okhttp3.OkHttpClient
-//import okhttp3.Request
-//import okhttp3.Response
-//import java.io.IOException
-//import java.net.URLEncoder
-
-//package com.myvet.myvet
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.os.Handler
-//import android.os.Looper
-//import android.util.Log
-//import android.widget.Button
-//import android.widget.EditText
-//import android.widget.TextView
-//import android.widget.Toast
-//import androidx.activity.OnBackPressedCallback
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.widget.addTextChangedListener
-//import com.firebase.geofire.GeoFireUtils
-//import com.firebase.geofire.GeoLocation
-//import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.firestore.FirebaseFirestore
-//import com.squareup.moshi.Json
-//import com.squareup.moshi.Moshi
-//import com.squareup.moshi.Types
-//import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-//import okhttp3.Call
-//import okhttp3.Callback
-//import okhttp3.OkHttpClient
-//import okhttp3.Request
-//import okhttp3.Response
-//import java.io.IOException
-//import java.net.URLEncoder
-//
-//
-//class VeterinarianSignUp2 : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_veterinarian_sign_up2)
-//
-//        val nextButton = findViewById<Button>(R.id.submitButton)
-//        nextButton.setOnClickListener {
-//            val intent = Intent(this, VetWindow::class.java) // החליפי לעמוד הסופי
-//            startActivity(intent)
-//            finish() // כדי שהמשתמש לא יוכל לחזור אחורה
-//        }
-//    }
-//}
-//
-//
-//
-
-
-
-
-
-
-
-
 package com.myvet.myvet
 
 import android.content.Intent
@@ -116,22 +29,11 @@ import okhttp3.Response
 import java.io.IOException
 import java.net.URLEncoder
 
-//
-//data class NominatimResult(
-//    @Json(name = "place_id") val placeId: Long,
-//    val lat: String,
-//    val lon: String,
-//    @Json(name = "display_name") val displayName: String,
-//    val type: String
-//)
-
 
 class VeterinarianSignUp2 : AppCompatActivity() {
-//    private lateinit var clinicName: EditText
-//    private lateinit var clinicLocation: EditText
-//    private lateinit var yearsOfExperience: EditText
-        private lateinit var expertise: EditText
+    private lateinit var expertise: EditText
     private lateinit var aboutMe: EditText
+
     private lateinit var register: Button
     private lateinit var errorMessage: TextView
 
@@ -149,9 +51,6 @@ class VeterinarianSignUp2 : AppCompatActivity() {
             }
         })
 
-//        clinicName = findViewById(R.id.clinicName)
-//        clinicLocation = findViewById(R.id.clinicLocation)
-//        yearsOfExperience = findViewById(R.id.yearsOfExperience)
         expertise = findViewById(R.id.expertise)
         aboutMe = findViewById(R.id.aboutMe)
         register = findViewById(R.id.submitButton)
@@ -161,20 +60,8 @@ class VeterinarianSignUp2 : AppCompatActivity() {
         val user = FirebaseAuth.getInstance().currentUser
 
         //Set the register button to be disabled
-        register.isEnabled = false
+//        register.isEnabled = false
 
-        //Function to check if the user has typed in all the information needed for the registration
-//        fun checkInputs() {
-//            val clinicName = clinicName.text.toString()
-//            val clinicLocation = clinicLocation.text.toString()
-//            val expertise = yearsOfExperience.text.toString()
-//            register.isEnabled =
-//                clinicName.isNotEmpty() && clinicLocation.isNotEmpty() && expertise.isNotEmpty()
-//        }
-
-//        clinicName.addTextChangedListener { checkInputs() }
-//        clinicLocation.addTextChangedListener { checkInputs() }
-//        yearsOfExperience.addTextChangedListener { checkInputs() }
 
         register.setOnClickListener {
             val moshi = Moshi.Builder()
@@ -185,7 +72,7 @@ class VeterinarianSignUp2 : AppCompatActivity() {
 
             val url = "https://nominatim.openstreetmap.org/search?q=${
                 URLEncoder.encode(
-//                    clinicLocation.text.toString(),
+
                     "UTF-8"
                 )
             }&format=json&limit=1"
@@ -229,14 +116,13 @@ class VeterinarianSignUp2 : AppCompatActivity() {
                             val userData = hashMapOf(
                                 "type" to "vet",
                                 "name" to user!!.displayName,
-//                                "clinicName" to clinicName.text.toString(),
                                 "clinicAddress" to result.displayName,
                                 "clinicAddressId" to result.placeId,
                                 "clinicLat" to result.lat,
                                 "clinicLon" to result.lon,
                                 "clinicGeohash" to GeoFireUtils.getGeoHashForLocation(GeoLocation(result.lat.toDouble(), result.lon.toDouble())),
                                 "expertise" to expertise.text.toString(),
-//                                "yearsOfExperience" to yearsOfExperience.text.toString(),
+
                                 "aboutMe" to aboutMe.text.toString(),
                             )
 
