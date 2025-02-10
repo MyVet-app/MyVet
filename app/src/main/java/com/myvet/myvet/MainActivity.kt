eee7c56e25f158284b631ff6f097b4397fcd4ae9
rony.levy1 <roniki04@gmail.com>
2025-02-10 10:29:41 +0200

    add  custom auth screen layout

--- app/src/main/java/com/myvet/myvet/MainActivity.kt
+++ app/src/main/java/com/myvet/myvet/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.EmailBuilder
 import com.firebase.ui.auth.AuthUI.IdpConfig.FacebookBuilder
@@ -21,9 +22,16 @@ class MainActivity : AppCompatActivity() {
     private var signInLauncher: ActivityResultLauncher<Intent>? = null
 
     private fun authFlow() {
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.auth_window)
+            .setGoogleButtonId(R.id.googleButton)
+            .setEmailButtonId(R.id.emailButton)
+            .setFacebookButtonId(R.id.facebookButton)
+            .setTosAndPrivacyPolicyId(R.id.tosAndPp)
+            .build()
+
         val signInIntent = AuthUI.getInstance()
             .createSignInIntentBuilder()
-            .setLogo(R.drawable.icon_logo)
+            .setAuthMethodPickerLayout(customLayout)
             .setTosAndPrivacyPolicyUrls(
                 "https://www.freeprivacypolicy.com/live/67168b52-bccb-4544-b878-711f6943de60",
                 "https://www.freeprivacypolicy.com/live/67168b52-bccb-4544-b878-711f6943de60"
