16b0ebc7377393e149875388e3973949cce08e0d
meshs1 <191111348+meshs1@users.noreply.github.com>
2025-02-23 21:43:28 +0200

    Add notification code

--- app/src/main/java/com/myvet/myvet/FirebaseCloudMessagingService.kt
+++ app/src/main/java/com/myvet/myvet/FirebaseCloudMessagingService.kt
@@ -1,9 +1,14 @@
 package com.myvet.myvet
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FirebaseCloudMessagingService : FirebaseMessagingService() {
     companion object {
@@ -37,59 +42,57 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
             Log.w(TAG, "User not signed in; cannot save FCM token.")
         }
     }
-//
-//    override fun onMessageReceived(message: RemoteMessage) {
-//        super.onMessageReceived(message)
-//        Log.d(TAG, "From: ${message.from}")
-//
-//        // Check if message contains a data payload.
-//        if (message.data.isNotEmpty()) {
-//            Log.d(TAG, "Message data payload: ${message.data}")
-//            handleDataMessage(message.data)
-//        }
-//        message.notification?.let { notification ->
-//            Log.d(TAG, "Message Notification Body: ${notification.body}")
-//            // Process notification data
-//            sendNotification(notification.title, notification.body)
-//        }
-//    }
-//
-//    private fun handleDataMessage(data: Map<String, String>) {
-//        // Handle data payload
-//        // Example:
-//        val title = data["title"] ?: "FCM Data Message"
-//        val message = data["message"] ?: "No message content"
-//        Log.d(TAG, "handleDataMessage - title: $title, message: $message")
-//        sendNotification(title, message)
-//    }
-//
-//    private fun sendNotification(title: String?, messageBody: String?) {  // Use nullable Strings
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d(TAG, "From: ${message.from}")
+
+        // Check if message contains a data payload.
+        if (message.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${message.data}")
+            handleDataMessage(message.data)
+        }
+        message.notification?.let { notification ->
+            Log.d(TAG, "Message Notification Body: ${notification.body}")
+            // Process notification data
+            displayNotification(notification.title, notification.body)
+        }
+    }
+
+    private fun handleDataMessage(data: Map<String, String>) {
+        // Handle data payload
+        // Example:
+        val title = data["title"] ?: "FCM Data Message"
+        val message = data["message"] ?: "No message content"
+        Log.d(TAG, "handleDataMessage - title: $title, message: $message")
+        displayNotification(title, message)
+    }
+
+    private fun displayNotification(title: String?, messageBody: String?) {  // Use nullable Strings
 //        val intent = Intent(this, MainActivity::class.java) // Replace with the main activity
 //        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 //        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
 //            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
-//
-//        val channelId = "fcm_default_channel"  // Replace
-//
+
+        val channelId = "myvet_notification_channel"  // Replace
+
 //        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-//            .setSmallIcon(android.R.drawable.ic_dialog_info)  // Replace
-//            .setContentTitle(title ?: "FCM Notification")  // Use provided title or a default one
-//            .setContentText(messageBody)
-//            .setAutoCancel(true)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.icon_logo)  // Replace
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
 //            .setSound(defaultSoundUri)
 //            .setContentIntent(pendingIntent)
 //            .setColor(ContextCompat.getColor(this, android.R.color.holo_blue_bright))  // Replace
-//
-//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//
-//        // Since android Oreo notification channel is needed.
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val channel = NotificationChannel(channelId,
-//                "Default channel",  // User-visible name
-//                NotificationManager.IMPORTANCE_DEFAULT)
-//            notificationManager.createNotificationChannel(channel)
-//        }
-//        notificationManager.notify(0, notificationBuilder.build())
-//    }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        val channel = NotificationChannel(channelId,
+            "MyVet Notification Channel",  // User-visible name
+            NotificationManager.IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(channel)
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 }
